8db0920c0aa40e466ffc8437f94c6ca727197838
Om Suresh Javir <javirom07042003@gmail.com>
2022-11-14 21:59:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const d = new Date();
+
+  const [time, setTime] = useState(Date.now());
+
+  const week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "April",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="time">
+        <span>
+          {d.getHours() <= 9 && d.getHours() >= 0
+            ? "0" + d.getHours()
+            : d.getHours()}
+        </span>
+        <span>:</span>
+        <span>
+          {d.getMinutes() <= 9 && d.getMinutes() >= 0
+            ? "0" + d.getMinutes()
+            : d.getMinutes()}
+        </span>
+        <span>:</span>
+        <span className="blink-text">
+          {d.getSeconds() <= 9 && d.getSeconds() >= 0
+            ? "0" + d.getSeconds()
+            : d.getSeconds()}
+        </span>
+      </div>
+      <div className="date">
+        <span>
+          {d.getDate()} {months[d.getMonth()]} {d.getFullYear()}
+        </span>
+        , <span>{week[d.getDay()]}</span>
+      </div>
     </div>
   );
 }
